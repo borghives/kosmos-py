d76e5bc161a61841b935582ad2e700c76d90c9ce
Ed Dao <ed@intellogic.com>
2026-06-03 00:09:40 -0700

    Remove pandas, polars and pyarrow dependencies
    remove-insert-dataframe-methods-from-persistable-and-ledger-classes

--- kosmos/space/persistable.py
+++ kosmos/space/persistable.py
@@ -2,9 +2,6 @@ from pydantic import Field
 from kosmos.meta.expression.filter import QueryPredicates
 from kosmos.meta.annotation import TimeInserted, TimeUpdated
 from kosmos.matter.particle import ParticleBase
-import pandas as pd
-import polars as pl
-import pyarrow
 from .detector import detect
 from .recorder import recorder
 
@@ -32,18 +29,6 @@ class Persistable(ParticleBase):
     
     def persist(self):
         self.recorder().record(self)
-    
-    def insert_dataframe(
-        self, 
-        dataframe: pd.DataFrame | pl.DataFrame | pyarrow.Table, 
-        chunk_size: int = 1000
-    ):
-        self.recorder().insert_dataframe(dataframe, chunk_size)
 
     async def persist_async(self):
         await self.recorder().record_async(self)
-
-    async def insert_dataframe_async(
-        self, dataframe: pd.DataFrame | pl.DataFrame | pyarrow.Table, chunk_size: int = 1000
-    ):
-        await self.recorder().insert_dataframe_async(dataframe, chunk_size)
